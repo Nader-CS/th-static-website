74ec9dbe17c168ab6f97fcbf3f104f9b0dc315f1
Nader <naderabdalhamed93@gmail.com>
2023-09-14 20:29:56 +0300

    changing dictionary folder to locales

--- app/[locale]/layout.js
+++ app/[locale]/layout.js
@@ -17,7 +17,7 @@ export const metadata = {
 export default async function RootLayout({ children, params: { locale } }) {
   let messages;
   try {
-    messages = (await import(`../../dictionary/${locale}.json`)).default;
+    messages = (await import(`../../locales/${locale}.json`)).default;
   } catch (error) {
     notFound();
   }
